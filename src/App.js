820bf09d5c6d1c642cdc452492e6503991af669a
Zaryab Ikram <zaryabsheikh0@gmail.com>
2023-07-13 00:03:35 +0500

    Product List page

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import Counter from './features/counter/Counter';
+import './App.css';
+
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+  <div> 
+ 
+    <Home/>
+
+
+
+  </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import Navbar from "../features/navbar/Navbar"
+import ProductList from "../features/productList/ProductList"
+
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar>
+     <ProductList/>
+        </Navbar>
+
+
+    </div>
+  )
+}
+
+export default Home
