94d23ba59cc83bb91e25cdf17207adf3b836e20f
Zaryab Ikram <zaryabsheikh0@gmail.com>
2023-07-13 16:25:46 +0500

    Routing set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 // import Counter from './features/counter/Counter';
 import './App.css';
+import SignUp from './features/auth/components/SignUp';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+
 
 function App() {
+
+ 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home/>)
+  },
+  {
+    path: "/login",
+    element: (<LoginPage/>)
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage/>
+  },
+
+]);
+
   return (
   <div> 
  
-    <Home/>
+ <RouterProvider router={router} />
+   
 
 
 
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import Login from "../features/auth/components/Login"
+
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login/>
+    </div>
+  )
+}
+
+export default LoginPage
